ca9610c56aaf7c99d25a6f83a4a8beefd5c5f447
izilongwan <js.nb@qq.com>
2024-12-27 14:24:26 +0800

    refactor: ♻️ 修复错误

--- src/main/java/com/demo/config/Knife4jConfig.java
+++ src/main/java/com/demo/config/Knife4jConfig.java
@@ -6,25 +6,26 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
+@EnableOpenApi
 public class Knife4jConfig {
 
     @Bean
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
+                .enable(true)
                 .select()
                 // 这里指定Controller扫描包路径
-                .apis(RequestHandlerSelectors.basePackage("com.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.demo.controller"))
                 .paths(PathSelectors.any())
-                .build()
-                .apiInfo(getApiInfo());
+                .build();
     }
 
     private ApiInfo getApiInfo() {
