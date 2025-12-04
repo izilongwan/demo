e1921e6cd840746a30a4bd2d7be1ab329e664f59
izilongwan <js.nb@qq.com>
2025-12-04 21:07:43 +0800

    refactor: 🔧 清理WebReportController中的未使用导入

--- src/main/java/com/demo/controller/WebReportController.java
+++ src/main/java/com/demo/controller/WebReportController.java
@@ -1,21 +1,17 @@
 package com.demo.controller;
 
-import java.util.List;
-
 import javax.annotation.Resource;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.demo.domain.entity.WebReport;
 import com.demo.service.WebReportService;
-import com.demo.service.impl.WebReportImpl;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("webreport")
 @RestController
